0cc1d213e03462851138c814bda2eb515bb62df5
Luisa <luisasantoro98@gmai.com>
2025-06-22 20:12:29 +0200

    eliminadotenv

--- src/main/java/com/isa/notizie_finanza/config/AwsProperties.java
+++ src/main/java/com/isa/notizie_finanza/config/AwsProperties.java
@@ -2,31 +2,22 @@ package com.isa.notizie_finanza.config;
 
 import org.springframework.context.annotation.Configuration;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @Configuration
 public class AwsProperties {
 
-    private final Dotenv dotenv = Dotenv.configure()
-            .directory("./")         // directory in cui si trova .env
-            .filename(".env")        // nome del file .env
-            .load();
-
     public String getAccessKey() {
-        return dotenv.get("AWS_ACCESS_KEY");
+        return System.getenv("AWS_ACCESS_KEY_ID");
     }
 
     public String getSecretKey() {
-        return dotenv.get("AWS_SECRET_KEY");
+        return System.getenv("AWS_SECRET_ACCESS_KEY");
     }
 
     public String getRegion() {
-        return dotenv.get("AWS_REGION");
+        return System.getenv("AWS_REGION");
     }
 
     public String getS3BucketName() {
-        return dotenv.get("AWS_BUCKET_NAME");
+        return System.getenv("AWS_BUCKET_NAME");
     }
 }
-
-
